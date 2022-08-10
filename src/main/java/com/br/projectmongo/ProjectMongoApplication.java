b7d8b7ce7be84f23961c49c48a0b2b1102f895ad
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-10 17:07:00 -0300

    project created

---
+++ src/main/java/com/br/projectmongo/ProjectMongoApplication.java
@@ -0,0 +1,13 @@
+package com.br.projectmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/projectmongo/ProjectMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projectmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
