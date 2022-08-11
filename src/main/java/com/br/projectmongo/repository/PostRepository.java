b51080555a5087b06df967500c326e9a61b7a428
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-11 08:50:35 -0300

    creating post with enchanced user

---
+++ src/main/java/com/br/projectmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.br.projectmongo.repository;
+
+import com.br.projectmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
