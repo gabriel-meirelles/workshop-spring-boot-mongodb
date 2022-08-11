1b4933174228b381ff6316279e43222854bcf474
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-11 08:14:44 -0300

    resource to insert user created

--- src/main/java/com/br/projectmongo/services/UserService.java
+++ src/main/java/com/br/projectmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.br.projectmongo.services;
 
 import com.br.projectmongo.domain.User;
+import com.br.projectmongo.dto.UserDTO;
 import com.br.projectmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,17 @@ public class UserService {
         Optional<User> user = repository.findById(id);
         return user
                 .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User user){
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO dto){
+        return new User(
+                dto.getId(),
+                dto.getName(),
+                dto.getEmail()
+        );
     }
 }
