4fba8d5c5a1f20383202a5da021869df2135e089
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-24 17:55:27 -0300

    implementing search with querymethod

--- src/main/java/com/br/projectmongo/resources/PostResources.java
+++ src/main/java/com/br/projectmongo/resources/PostResources.java
@@ -1,13 +1,13 @@
 package com.br.projectmongo.resources;
 
 import com.br.projectmongo.domain.Post;
+import com.br.projectmongo.resources.util.URL;
 import com.br.projectmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -16,10 +16,17 @@ public class PostResources {
     @Autowired
     private PostService service;
 
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titleSearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "")
+                                                  String text) {
+        text = URL.decodeParam(text);
+        List<Post> post = service.findByTitle(text);
         return ResponseEntity.ok().body(post);
     }
 
