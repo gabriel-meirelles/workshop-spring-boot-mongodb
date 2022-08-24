a9cc7c50ef2be29346d875e89bc09ce4a4a4cedf
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-24 16:25:13 -0300

    endpoint to return post from users

--- src/main/java/com/br/projectmongo/resources/UserResources.java
+++ src/main/java/com/br/projectmongo/resources/UserResources.java
@@ -1,5 +1,6 @@
 package com.br.projectmongo.resources;
 
+import com.br.projectmongo.domain.Post;
 import com.br.projectmongo.domain.User;
 import com.br.projectmongo.dto.UserDTO;
 import com.br.projectmongo.services.UserService;
@@ -34,6 +35,12 @@ public class UserResources {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
     }
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
+    }
+
 
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
