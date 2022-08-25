08122b98ca7138b9827611cd1ee6ad212d1138e4
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-25 15:02:21 -0300

    mongodb query with more params

--- src/main/java/com/br/projectmongo/resources/PostResources.java
+++ src/main/java/com/br/projectmongo/resources/PostResources.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -34,6 +35,19 @@ public class PostResources {
                                                   String text) {
         text = URL.decodeParam(text);
         List<Post> post = service.searchtitle(text);
+        return ResponseEntity.ok().body(post);
+    }
+    @GetMapping(value = "/fullSearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "")
+                                                  String text,
+                                                 @RequestParam(value = "minDate", defaultValue = "")
+                                                 String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "")
+                                                     String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> post = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(post);
     }
 
--- src/main/java/com/br/projectmongo/services/PostService.java
+++ src/main/java/com/br/projectmongo/services/PostService.java
@@ -1,11 +1,11 @@
 package com.br.projectmongo.services;
 
 import com.br.projectmongo.domain.Post;
-import com.br.projectmongo.domain.User;
 import com.br.projectmongo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,15 +21,21 @@ public class PostService {
                 .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public List<Post> findByTitle(String text){
+    public List<Post> findByTitle(String text) {
         return repository.findByTitleContainingIgnoreCase(text);
     }
+
     /*
 
-    *method using query mongoDb
-    **/
-    public List<Post> searchtitle(String text){
+     *method using query mongoDb
+     **/
+    public List<Post> searchtitle(String text) {
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 
 }
